3c3fbe08e2d14ca87905864977552ef98e95b8ce
ricfernandes15 <74026037+ricfernandes15@users.noreply.github.com>
2021-01-10 15:40:16 +0000

    Add files via upload

--- Lab4/Parte 4/frutaria.py
+++ Lab4/Parte 4/frutaria.py
@@ -54,13 +54,7 @@ def atualizar_stock(stock, cestos):
                         stock.remove(produto)
     return stock
             
-if __name__ == "__main__":
-    stock = ler_stock("stock.csv")
-    print("stock inicial", stock, "\n")
-    cesto = gerar_cesto(stock,1,6)
-    print("Cesto:", cesto, "\n")    
-    stock = atualizar_stock(stock,cesto)
-    print("Stock atualizado", stock, "\n")
+
     
     
     
