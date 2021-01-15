2399877d2f99e67687aace856b734d2f3641bb82
Ricardo <30007613@students.ual.pt>
2021-01-15 14:58:15 +0000

    projecto

---
+++ Projeto/controller/game_controller.py
@@ -0,0 +1,29 @@
+def regista_jogador(estado_jogo,nome):
+    pass
+
+def remover_jogador(estado_jogo,nome):
+    pass
+
+def listar_jogadores(estado_jogo):
+    pass
+
+def iniciar_jogo(estado_jogo,nome1,nome2,comprimento,altura,tamanho_sequencia,tamanho_pecas):
+    pass
+
+def detalhes_jogo(estado_jogo):
+    pass
+
+def desistir(estado_jogo,nome1,nome2=" "):
+    pass
+
+def coloca_peca(estado_jogo,nome,tamanho_peca,posicao,sentido="E"):
+    pass
+
+def mostra_resultado(estado_jogo):
+    pass
+
+def grava_ficheiro(estado_jogo,nome_ficheiro):
+    pass
+
+def le_ficheiro(estado_jogo,nome_ficheiro):
+    pass
---
+++ Projeto/models/game_status.py
@@ -0,0 +1,25 @@
+def cria_estado():
+    return {
+        "jogadores": [
+            {
+            "nome": str,
+            "nr_vitorias": int,
+            "nr_jogos": int,
+             }
+                    ],
+        "estado":[
+            {
+            "em curso":str,
+            "jogador1":str,
+            "jogador2":str,
+            "comprimento":int,
+            "altura":int,
+            "tamanho_sequencia":int,
+            "tamanho_peças_especiais": [],
+            "tabuleiro":[][],
+            "vencedor":str,      
+            }
+            ]     
+        }
+
+
