c679c05659e075ebb8ef5f052f062130a5606daf
ricfernandes15 <74026037+ricfernandes15@users.noreply.github.com>
2021-01-29 20:08:00 +0000

    Add files via upload

--- Projeto/views/cli.py
+++ Projeto/views/cli.py
@@ -1,11 +1,11 @@
-import controller as con
-import views as v
+from controller import game_controller as con
+from models import game_status as mod
 
-if __name__ == "__main__":
-    estado_jogo = v.cria_estado()
-    instrucao = input("Insira a instrucao:")
-    instrucao = instrucao.split(" ")
+def cli():
+    estado_jogo = mod.cria_estado()
     while True:
+        instrucao = input()
+        instrucao = instrucao.split(" ")
         if(instrucao[0] == "RJ"):
             con.regista_jogador(estado_jogo,instrucao[1])
         if(instrucao[0] == "EJ"):
@@ -13,18 +13,18 @@ if __name__ == "__main__":
         if(instrucao[0] == "LJ"):
             con.listar_jogadores(estado_jogo)
         if(instrucao[0] == "IJ"):
-            segunda_linha= input()
+            segunda_linha = input()
             segunda_linha = segunda_linha.split(" ")
             tamanho_pecas = input()
             tamanho_pecas = tamanho_pecas.split(" ")
-            con.iniciar_jogo(estado_jogo,instrucao[1],instrucao[2],segunda_linha[0],segunda_linha[1],segunda_linha[2], tamanho_pecas)
+            estado_jogo = con.iniciar_jogo(estado_jogo,instrucao[1],instrucao[2],int(segunda_linha[0]),int(segunda_linha[1]),int(segunda_linha[2]), tamanho_pecas)
         if(instrucao[0] == "DJ"): 
             con.detalhes_jogo(estado_jogo)
         if(instrucao[0] == "D"):
-            if(instrucao[2]):
-                con.desiste(estado_jogo,instrucao[1],instrucao[2])
+            if(len(instrucao) == 3):
+                estado_jogo = con.desistir(estado_jogo,instrucao[1],instrucao[2])
             else:
-                con.desistir(estado_jogo,instrucao[1])
+                estado_jogo = con.desistir(estado_jogo,instrucao[1])
         if(instrucao[0] == "CP"):
             if(instrucao[2] == "1"):
                 con.coloca_peca(estado_jogo,instrucao[1],instrucao[2],instrucao[3])
@@ -33,7 +33,8 @@ if __name__ == "__main__":
         if(instrucao[0] == "V"):        
             con.mostra_resultado(estado_jogo)
         if(instrucao[0] == "G"):
-             con.grava_ficheiro(estado_jogo,instrucao[1])
+            con.grava_ficheiro(estado_jogo,instrucao[1])
         if(instrucao[0] == "L"):
-            con.le_ficheiro(estado_jogo,instrucao[1])
-             
+            estado_jogo = con.le_ficheiro(instrucao[1])
+        # else:
+        #     print ("Instrução inválida.")
